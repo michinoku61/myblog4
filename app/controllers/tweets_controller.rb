b12511a5f3bbe90dfb00d8d1535156c5cc6f6d10
みちのく61 <michinoku61@gmail.com>
2019-11-29 09:44:51 +0900

    ビュー修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,8 @@
 class TweetsController < ApplicationController
+  before_action :login_check, except: [:index, :show, :top]
+  def top
+  end
+
   def index
     @tweets = Tweet.all
 
@@ -6,15 +10,24 @@ class TweetsController < ApplicationController
   end
 
   def new
+    redirect_to action: :index unless user_signed_in?
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def create
-    @tweet = Tweet.create(name: tweet_params[:name],text: tweet_params[:text], image: tweet_params[:image])
+    @tweet = Tweet.create(name: tweet_params[:name],text: tweet_params[:text], image: tweet_params[:image],  user_id: current_user.id)
     redirect_to tweets_path
   end
 
   private
   def tweet_params
     params.permit(:name, :text, :image)
+  end
+
+  def login_check
+    redirect_to "/users/sign_in" unless user_signed_in?
   end
 end
